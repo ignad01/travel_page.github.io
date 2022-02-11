0aff4ca5f7f9f3584d78ff01a75f36eca4ec50bf
PIYUSH DANGI <80493972+ignad01@users.noreply.github.com>
2022-02-11 21:41:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const menutoggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menutoggle.addEventListener('click', ()=>{
+    menutoggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+});
